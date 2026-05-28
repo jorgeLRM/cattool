e5d8544f8daba3e8a8784c817eb98edc2d4ff78c
jorgeLRM <jorge.lrm99@gmail.com>
2026-05-28 00:23:42 -0600

    refactor input handling and copy functions for improved error management and line numbering

--- main.go
+++ main.go
@@ -1,40 +1,105 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"os"
 )
 
 type Input interface {
-	Open() (io.Reader, error)
+	Open() (io.ReadCloser, error)
 }
 
 type FileInput struct {
 	Path string
 }
 
-func (f FileInput) Open() (io.Reader, error) {
+func (f FileInput) Open() (io.ReadCloser, error) {
 	file, err := os.Open(f.Path)
 	if err != nil {
-		return nil, errors.New(f.Path + ": No such file or directory")
+		return nil, err
 	}
 	return file, nil
 }
 
 type StdinInput struct{}
 
-func (s StdinInput) Open() (io.Reader, error) {
-	return os.Stdin, nil
+func (s StdinInput) Open() (io.ReadCloser, error) {
+	return io.NopCloser(os.Stdin), nil
 }
 
-func resolve(s []string) []Input {
-	if len(s) == 0 {
+type CopyFunc func(dst io.Writer, src io.Reader) error
+
+func copyPlain(dst io.Writer, src io.Reader) error {
+	_, err := io.Copy(dst, src)
+	return err
+}
+
+func copyWithLineNumbers(dst io.Writer, src io.Reader) error {
+	reader := bufio.NewReader(src)
+	number := 1
+	for {
+		line, readErr := reader.ReadString('\n')
+
+		if len(line) > 0 {
+			fmt.Fprintf(dst, "%6d\t%s", number, line)
+		}
+
+		number++
+
+		if readErr != nil {
+			if errors.Is(readErr, io.EOF) {
+				return nil
+			}
+			return readErr
+		}
+	}
+}
+
+func copyWithNonBlankLineNumbers(dst io.Writer, src io.Reader) error {
+	reader := bufio.NewReader(src)
+	number := 1
+	for {
+		line, readErr := reader.ReadString('\n')
+
+		if len(line) > 0 {
+			if line == "\n" {
+				fmt.Fprint(dst, line)
+			} else {
+				fmt.Fprintf(dst, "%6d\t%s", number, line)
+				number++
+			}
+		}
+
+		if readErr != nil {
+			if errors.Is(readErr, io.EOF) {
+				return nil
+			}
+			return readErr
+		}
+	}
+}
+
+func chooseCopyFn(nFlag, bFlag bool) CopyFunc {
+	if bFlag {
+		return copyWithNonBlankLineNumbers
+	}
+	if nFlag {
+		return copyWithLineNumbers
+	}
+	return copyPlain
+}
+
+func buildInputs(args []string) []Input {
+	if len(args) == 0 {
 		return []Input{StdinInput{}}
 	}
 
-	var inputs []Input
-	for _, arg := range s {
+	inputs := make([]Input, 0, len(args))
+	for _, arg := range args {
 		if arg == "-" {
 			inputs = append(inputs, StdinInput{})
 		} else {
@@ -44,20 +109,28 @@ func resolve(s []string) []Input {
 	return inputs
 }
 
+func processInput(input Input, copyFn CopyFunc) error {
+	reader, err := input.Open()
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	return copyFn(os.Stdout, reader)
+}
+
 func main() {
-	inputs := resolve(os.Args[1:])
+	var nFlag, bFlag bool
+	flag.BoolVar(&nFlag, "n", false, "number all output lines")
+	flag.BoolVar(&bFlag, "b", false, "number non-empty output lines")
+	flag.Parse()
+
+	copyFn := chooseCopyFn(nFlag, bFlag)
+	inputs := buildInputs(flag.Args())
 
 	for _, input := range inputs {
-		reader, err := input.Open()
-		if err != nil {
-			os.Stderr.WriteString("kitten: " + err.Error() + "\n")
-			continue
-		}
-
-		_, err = io.Copy(os.Stdout, reader)
-		if err != nil {
-			os.Stderr.WriteString("kitten: " + err.Error() + "\n")
-			continue
+		if err := processInput(input, copyFn); err != nil {
+			fmt.Fprintf(os.Stderr, "cccat: %s\n", err)
 		}
 	}
 }
